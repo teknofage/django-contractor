a202f895db0dce7e1d50024c91b4ba744e6e5397
teknofage <teknofage@gmail.com>
2020-03-05 12:52:00 -0800

    add everything fix github problems

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from .forms import ProfileSetupForm
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, TemplateView
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 class SignUpView(CreateView):
@@ -10,19 +12,28 @@ class SignUpView(CreateView):
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
     
-    # def get():
-    #     return render_template('registration/signup.html')'
     
-    # def signup(request):
-    #     if request.method == 'POST':
-    #         form = UserCreationForm(request.POST)
-    #         if form.is_valid():
-    #             form.save()
-    #             username = form.cleaned_data.get('username')
-    #             raw_password = form.cleaned_data.get('password1')
-    #             user = authenticate(username=username, password=raw_password)
-    #             login(request, user)
-    #             return redirect('home')
-    #     else:
-    #         form = UserCreationForm()
-    #     return render(request, 'registration/signup.html', {'form': form})
+class LogInView(TemplateView):
+    template_name = 'registration/login.html'
+    success_url = reverse_lazy('profile-home')
+    
+class ProfileSetupView(CreateView):
+    template_name = 'registration/profile-setup.html'
+    form_class = ProfileSetupForm
+    success_url = reverse_lazy('login')
+    
+    def post(self, request):
+        form = ProfileSetupForm(request.POST)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = request.user
+            profile.save()
+            return HttpResponseRedirect(reverse('home-view'))
+        print("smarties")
+        return render(request, 'registration/profile-setup.html', context={"form":form})
+    
+    def form_valid(self, form):
+        return True
+    
+class HomeView(TemplateView):
+    template_name = 'registration/home-view.html'
