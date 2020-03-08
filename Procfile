9425e7b20a7aa536ae112c01b95529f1fcc8a60a
teknofage <teknofage@gmail.com>
2020-03-07 23:26:26 -0800

    first deployment push

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn fengshui.wsgi --log-file -
--- fengshui/settings.py
+++ fengshui/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'g$cl(7l!udh#ctv5ki@we3zs1aifp(k7$k80djxa@1868w+&1+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'peachblossomlovecalculatorapp.herokuapp.com'
+    
+]
 
 
 # Application definition
@@ -128,3 +130,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
