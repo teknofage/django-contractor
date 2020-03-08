27ebf55473aa32d138e0403824c90badfbea350f
teknofage <teknofage@gmail.com>
2020-03-08 04:27:40 -0700

    deploying from virtual env

--- fengshui/settings.py
+++ fengshui/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
